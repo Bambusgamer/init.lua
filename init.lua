463e0d4c34cfc0362c1f03ad5ec5f7b27a0e4305
Vincent <laucknerv2@gmail.com>
2024-09-07 15:27:26 +0200

    trying to make packer install first

--- init.lua
+++ init.lua
@@ -1,1 +1,31 @@
 require("vincent")
+
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+if packer_bootstrap then
+    require('packer').sync()
+end
+
+-- return require('packer').startup(function(use)
+--   -- Add your plugins here, for example:
+--   use 'wbthomason/packer.nvim' -- Packer manages itself
+--
+--   -- More plugins here
+--   -- use 'nvim-treesitter/nvim-treesitter'
+--
+--   if packer_bootstrap then
+--     require('packer').sync()
+--   end
+-- end)
+
